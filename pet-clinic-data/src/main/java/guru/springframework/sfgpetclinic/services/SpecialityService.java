53322cdd8bf0daa0a4a55808f42ff9c2418f21f5
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-05-31 08:19:44 +0200

    Create Speciality Map Service Impl. Closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * This interface represents the Speciality entities management
+ * in the service layer.
+ *
+ * To be implemented.
+ *
+ * @author  Ernesto A. Rodriguez Acosta
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/impl/SpecialityMapService.java
@@ -0,0 +1,43 @@
+package guru.springframework.sfgpetclinic.services.map.impl;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.SpecialityService;
+import guru.springframework.sfgpetclinic.services.map.AbstractMapService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Specialityhis class represents the map service implementation
+ * to manage the Speciality entities.
+ *
+ * @author  Ernesto A. Rodriguez Acosta
+ */
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
