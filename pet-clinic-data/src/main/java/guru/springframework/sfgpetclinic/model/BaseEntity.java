40a2d9cf2aca9840446b186bea24770f3e833f6f
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-06-06 12:02:38 +0200

    Create Base Entity. Closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,17 +1,22 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
- * This class represents the base entity to be extended
- * for the entities of our data layer.
- *
- * To be extended.
+ * This class represents a simple java bean domain with an id property.
+ * Used as a base class for objects needing this property.
  *
  * @author Ernesto A. Rodriguez Acosta
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
 
     public Long getId() {
